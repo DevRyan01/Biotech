aba288bae436c57ebc5477b6cd1870001047daf5
GuhMascarenhass <gustavo_santos2548@hotmail.com>
2025-09-20 18:22:10 -0300

    arquivo base.css adicionado e telainicial herdando ele

--- biotech/apps.py
+++ biotech/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BiotechConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'biotech'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "biotech"
--- biotech/views.py
+++ biotech/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
+
 def telainicial(request):
-    return render(request, "biotech/telainicial.html")
+    return render(request, "telainicial.html")
